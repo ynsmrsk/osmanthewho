53b4387e995c376d269907eee11af266384c6050
Yunus Emre Işık <ynsmrsk13@gmail.com>
2022-07-23 02:28:28 +0300

    Fix animation bugs

--- components/animations/TextReveal.jsx
+++ components/animations/TextReveal.jsx
@@ -1,60 +1,60 @@
 import { useEffect, useRef } from "react"
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
-import useLayoutEffect from 'components/constants/UseIsomorphicLayoutEffect'
 
 gsap.registerPlugin(ScrollTrigger)
 
 export function TextReveal({ children }) {
 	const el = useRef(null)
 
-	useLayoutEffect(() => {
-		const duration = 0.7
-		const animations = []
-		const elements = gsap.utils.toArray(el.current.children)
-		const offset = elements[1].offsetTop - elements[0].offsetTop
-		elements.forEach((element) => {
-			const animation = gsap.to(element, {
-				opacity: 1,
-				scrollTrigger: {
-					trigger: element,
-					start: "center center+=" + (offset / 2),
-					end: "center center-=" + (offset / 2),
-					markers: true,
-					onEnter: () => {
-						gsap.to(element, {
-							y: -50,
-							duration,
-							opacity: 1,
-						})
-					},
-					onLeave: () => {
-						gsap.to(element, {
-							y: -100,
-							duration,
-							opacity: 0
-						})
-					},
-					onEnterBack: () => {
-						gsap.to(element, {
-							y: -50,
-							duration,
-							opacity: 1
-						})
-					},
-					onLeaveBack: () => {
-						gsap.to(element, {
-							y: 0,
-							duration,
-							opacity: 0
-						})
-					},
-				}
+	useEffect(() => {
+		setInterval(() => {
+			const duration = 0.7
+			const animations = []
+			const elements = gsap.utils.toArray(el.current.children)
+			const offset = elements[1].offsetTop - elements[0].offsetTop
+			elements.forEach((element) => {
+				const animation = gsap.to(element, {
+					opacity: 1,
+					scrollTrigger: {
+						trigger: element,
+						start: "center center+=" + (offset / 2),
+						end: "center center-=" + (offset / 2),
+						markers: true,
+						onEnter: () => {
+							gsap.to(element, {
+								y: -50,
+								duration,
+								opacity: 1,
+							})
+						},
+						onLeave: () => {
+							gsap.to(element, {
+								y: -100,
+								duration,
+								opacity: 0
+							})
+						},
+						onEnterBack: () => {
+							gsap.to(element, {
+								y: -50,
+								duration,
+								opacity: 1
+							})
+						},
+						onLeaveBack: () => {
+							gsap.to(element, {
+								y: 0,
+								duration,
+								opacity: 0
+							})
+						},
+					}
+				})
+
+				animations.push(animation)
 			})
-
-			animations.push(animation)
-		})
-
+		}, 4000)
 		return () => {
 			animations.forEach((animation) => animation.scrollTrigger.kill())
 		}
